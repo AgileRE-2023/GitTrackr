568187e9fbdae628b60d0c0b546fbb19180b78c2
Satrya Nararya <94729191+satryanararya@users.noreply.github.com>
2023-11-01 20:17:15 +0700

    fix: project setup

--- gittrackr/gittrackr/trackr_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.HomeView.as_view(), name="homepage"),
-    path("stat",views.StatRepo.as_view(), name="StatRepo"),
-]
