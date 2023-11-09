ffb670510fa40c3d27d052246270c36ef4b3af69
AirlanggaDS <airlangga.dwi.satrio-2021@fst.unair.ac.id>
2023-11-09 14:26:18 +0700

    Login with Google OAuth2 Successfully created

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
+from apps.accounts import views
 
 urlpatterns = [
     path("__reload__", include("django_browser_reload.urls")),
-    path('login/', views.login, name="login")
+    path('login/', views.login, name="login"),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('social-auth/', include('social_django.urls', namespace='social')),
 ]
