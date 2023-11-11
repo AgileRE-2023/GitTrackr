331cddcba095bf5ab47e2ed3bf86d6191fce5035
Najwa Almira <najwa.almira2005@gmail.com>
2023-11-11 12:14:10 +0700

    new settings.py for db

--- gittrackr/settings.py
+++ gittrackr/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'gittrackr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'GitTrackr',
+        'USER': 'root',
+        'PASSWORD': 'najnad1805',
+        'PORT': 3306,
+        'HOST': '127.0.0.1',
     }
 }
 
