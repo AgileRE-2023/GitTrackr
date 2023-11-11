e2d94004f9d50daaae871bbafe4bef48461942d4
Najwa Almira <najwa.almira2005@gmail.com>
2023-11-11 18:00:17 +0700

    try make new models.py in all folders for db

--- apps/utilities/models.py
+++ apps/utilities/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    UserID = models.IntegerField(primary_key=True)
+    Google_UserID = models.CharField(max_length=255)
+    Email = models.CharField(max_length=255)
+    Access_Token = models.CharField(max_length=255)
+    Refresh_Token = models.CharField(max_length=255)
+    Created_At = models.DateTimeField()
+    class Meta: 
+        db_table = 'Users'
+
+class Folders(models.Model):
+    FolderID = models.IntegerField(primary_key=True)
+    Folder_Name = models.CharField(max_length=255, unique=True)
+    Created_At = models.DateTimeField()
+    UserID = models.ForeignKey(Users, on_delete=models.CASCADE)
+    class Meta: 
+        db_table = 'Folders'
+
+class Repository(models.Model):
+    RepositoryID = models.IntegerField(primary_key=True)
+    Repository_Name = models.CharField(max_length=255, unique=True)
+    Url = models.TextField()
+    FolderID = models.ForeignKey(Folders, on_delete=models.CASCADE)
+    class Meta: 
+        db_table = 'Repository'
+
+class History(models.Model):
+    HistoryID = models.IntegerField(primary_key=True)
+    Timestap = models.DateTimeField()
+    UserID = models.ForeignKey(Users, on_delete=models.CASCADE)
+    FolderID = models.ForeignKey(Folders, on_delete=models.CASCADE)
+    class Meta: 
+        db_table = 'History'
