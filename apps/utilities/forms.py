35df170878528e37ad3cb95d07f4a35dfbd49d18
DavidChn05 <david.chandra0501@gmail.com>
2023-11-13 23:14:23 +0700

    addRepoProgress

--- apps/utilities/forms.py
+++ apps/utilities/forms.py
@@ -2,4 +2,8 @@
 from django import forms
 
 class AddRepositoryForm(forms.Form):
-    repository_name = forms.CharField(label='Repository Name', max_length=100)
+    repository_name = forms.CharField(
+        label='',
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'w-96 mx-4 border-2 rounded-xl', 'placeholder' : 'ex. DjangoEcommerce'},)
+        )
