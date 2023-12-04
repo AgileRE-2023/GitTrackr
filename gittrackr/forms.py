a9247b3114834a12d8670f78748849d062dd0405
AchSahal <achdeptiananda@gmail.com>
2023-12-04 17:31:21 +0700

    logged page responsive

--- gittrackr/forms.py
+++ gittrackr/forms.py
@@ -5,5 +5,5 @@ class CreateFolderForm(forms.Form):
     folder_name = forms.CharField(
         max_length=255,
         label='',
-        widget=forms.TextInput(attrs={'class': 'w-96 border-none focus:outline-none no-underline focus:border-none mx-5'})
+        widget=forms.TextInput(attrs={'class': 'w-full border-none focus:outline-none no-underline focus:border-none mx-5'})
     )
